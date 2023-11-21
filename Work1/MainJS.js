8aa51f72631ca593bb395f315765af1d51270332
bubavlad <137657847+bubavlad@users.noreply.github.com>
2023-11-21 16:08:09 +0200

    Initial commit

---
+++ Work1/MainJS.js
@@ -0,0 +1,56 @@
+
+console.log('------1.1.------')
+
+let str1 = 'hello'
+console.log(str1)
+let str2 = 'owu'
+console.log(str2)
+let str3 = 'com'
+console.log(str3)
+let str4 = 'ua'
+console.log(str4)
+let n1 = 1
+console.log(n1)
+let n2 = 10
+console.log(n2)
+let n3 = -999
+console.log(n3)
+let n4 = 123
+console.log(n4)
+let n5 = 3.14
+console.log(n5)
+let n6 = 2.7
+console.log(n6)
+let n7 = 16
+console.log(n7)
+let b1 = true
+console.log(b1)
+let b2 = false
+console.log(b2)
+
+console.log('------1.2.------')
+
+let firstName = 'Vlad'
+let middleName = 'Bubenko'
+let lastName = 'Olegovich'
+let person = (`${firstName} ${middleName} ${lastName}`)
+console.log(person)
+
+console.log('------1.3.------')
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+
+console.log('------1.4.------')
+
+let firstName1 = prompt('Введіть ваше імʼя')
+let middleName1 = prompt('Введіть ваше прізвище')
+let age = prompt('Введіть ваш вік')
+
+console.log(firstName1)
+console.log(middleName1)
+console.log(age)
